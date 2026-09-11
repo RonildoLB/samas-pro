3a9ec1650b5f1a80a1b9787f418c7dee3550c7a2
RonildoLB <ronildo_lacerda@hotmail.com>
2026-09-11 18:08:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'samas-pro',
+  webDir: 'www'
+};
+
+export default config;
